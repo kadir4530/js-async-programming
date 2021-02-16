2701d2cd271ea6feaad73d1c173744aebbdccb71
kadir4530 <57013243+kadir4530@users.noreply.github.com>
2021-02-16 12:43:39 +0200

    Init

---
+++ async-await.js
@@ -0,0 +1,78 @@
+// Veri tabanindaki verileri kullanici bazli filtrelemek
+
+let users = [
+    {
+        id: 1,
+        userName: "admin",
+        password: "123456",
+        isAdmin: true,
+
+    },
+    {
+        id: 2,
+        userName: "mustafa",
+        password: "123456",
+        isAdmin: false,
+    },
+    {
+        id: 3,
+        userName: "ahmet",
+        password: "123456",
+        isAdmin: false,
+    }
+]
+
+let data = [
+    {
+        title: "Data-1",
+        ownerID: 2,
+    },
+    {
+        title: "Data-2",
+        ownerID: 2,
+    },
+    {
+        title: "Data-3",
+        ownerID: 3,
+    },
+    {
+        title: "Data-4",
+        ownerID: 3,
+    }
+]
+
+function loginUser(userName, password) {
+    console.log("Checking user")
+    return new Promise((resolve, reject) => {
+        const user = users.find(user => user.userName === userName && user.password === password);
+        if (user)
+            resolve(user)
+
+        else
+            reject("Kullanici bulunumadi")
+    })
+}
+
+function getDataByUser(user) {
+    console.log("Getting data for user : " + user.userName)
+    setTimeout(() => {
+        if (user.isAdmin)
+            console.log(data)
+        else
+            console.log(data.filter(dt => dt.ownerID === user.id))
+
+    }, 2000);
+}
+
+async function init() {
+
+    const user = await loginUser("admin", "123456");
+    // const user = await loginUser("mustafa", "123456");
+    // const user = await loginUser("ahmet", "123456");
+
+         // User not found :
+    // const user = await loginUser("mert", "123456");
+    getDataByUser(user)
+}
+
+init();
---
+++ callback.js
@@ -0,0 +1,74 @@
+// Veri tabanindaki verileri kullanici bazli filtrelemek
+
+let users = [
+    {
+        id: 1,
+        userName: "admin",
+        password: "123456",
+        isAdmin: true,
+
+    },
+    {
+        id: 2,
+        userName: "mustafa",
+        password: "123456",
+        isAdmin: false,
+    },
+    {
+        id: 3,
+        userName: "ahmet",
+        password: "123456",
+        isAdmin: false,
+    }
+]
+
+let data = [
+    {
+        title: "Data-1",
+        ownerID: 2,
+    },
+    {
+        title: "Data-2",
+        ownerID: 2,
+    },
+    {
+        title: "Data-3",
+        ownerID: 3,
+    },
+    {
+        title: "Data-4",
+        ownerID: 3,
+    }
+]
+
+function loginUser(userName, password, callback) {
+    console.log("Checking user")
+    setTimeout(() => {
+        const user = users.find(user => user.userName === userName && user.password === password);
+
+        if (user)
+            callback(user)
+
+        else
+            console.log("Kullanici bulunumadi")
+    }, 2000);
+}
+
+function getDataByUser(user) {
+    console.log("Getting data for user : " + user.userName)
+    setTimeout(() => {
+        if (user.isAdmin)
+            console.log(data)
+        else
+            console.log(data.filter(dt => dt.ownerID === user.id))
+
+    }, 2000);
+}
+
+loginUser('admin', "123456", getDataByUser);
+// loginUser('mustafa', "123456", getDataByUser);
+// loginUser('ahmet', "123456", getDataByUser);
+
+        // User not found :
+// loginUser('mert', "123456", getDataByUser);
+
